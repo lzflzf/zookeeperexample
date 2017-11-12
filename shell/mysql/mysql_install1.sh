f7babdb264508a60c39d380524f582f5a307e0f2
peixunfahan <peixunfahan@peixunfahan-PC>
2017-11-12 10:56:33 +0800

    随机密码

--- shell/mysql/mysql_install1.sh
+++ shell/mysql/mysql_install1.sh
@@ -1,11 +1,13 @@
 /usr/local/mysql/support-files/mysql.server stop
 
-rootpasswd=lizhifeng ;
+rootpasswd=`echo \`date\` | md5sum | awk '{ print $1 }'` ;
+echo ${rootpasswd} > /root/.ssh/mysql_root_passwd
+chmod 600 /root/.ssh/mysql_root_passwd
 datadir=/opt/mysql/data63 ;
 
-
 # tar zxvf $1 -C /usr/local ;
-
+echo "PATH=\${PATH}:/usr/local/mysql/bin" >> /etc/profile
+source /etc/profile
 /usr/local/mysql/bin/mysql_install_db --datadir=${datadir} ;
 useradd -r -s /bin/false -M mysql ;
 chown mysql:mysql -R ${datadir} ;
@@ -15,14 +17,12 @@ echo "datadir=${datadir}" >> /etc/my.cnf ;
 echo "socket=${datadir}/mysql.sock" >> /etc/my.cnf ;
 echo "skip-grant-tables" >> /etc/my.cnf ;
 
-
 echo "[mysqld_safe]" >> /etc/my.cnf ;
 echo "log-error=${datadir}/mariadb.log" >> /etc/my.cnf ;
 echo "pid-file=${datadir}/mariadb.pid" >> /etc/my.cnf ;
 
 echo "[mysql]" >> /etc/my.cnf
 echo "socket=${datadir}/mysql.sock" >> /etc/my.cnf ;
-
 
 /usr/local/mysql/support-files/mysql.server start 1>mysql.out 2>mysql.out;
 
