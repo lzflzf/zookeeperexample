ecaf8b12c38d4649ca515c94e7fcbed9f771bc2c
peixunfahan <peixunfahan@peixunfahan-PC>
2017-11-12 13:08:30 +0800

    shell 执行mysql

--- shell/mysql/scan_csv.sh
+++ shell/mysql/scan_csv.sh
@@ -1,4 +1,4 @@
-### 遍历文件
+### 扫描csv文件
 
 datapath=/opt/data1
 
@@ -7,6 +7,7 @@ do
    echo ${dir}
    for filename in `ls ${datapath}/${dir}/ | grep csv$`
    do
+    #### mysql -hxxxxx -pxxxx -uxxxxx -pxxxxx -e "insert into lizhifeng.logs(aaa,bbb) values('aaaa','bbbb');"
    	sh import_csv_to_hive.sh ${datapath}/${dir}/${filename} ${filename}
    done
 done
