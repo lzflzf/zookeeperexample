621eba6fc7be6531754badc237b02b8511b29d43
peixunfahan <peixunfahan@peixunfahan-PC>
2017-11-12 12:05:18 +0800

    修改引号，发现其实很多地方并不需要引号

--- shell/mysql/import_csv_to_hive.sh
+++ shell/mysql/import_csv_to_hive.sh
@@ -2,46 +2,46 @@
 
 filename=$1
 
-lock="/opt/locks/"${2}".lock."`date +'%Y-%m-%d-%H-%M-%S'`
+lock=/opt/locks/${2}.lock.`date +%Y-%m-%d-%H-%M-%S`
 echo ${lock}
 
 ## 先检测文件是否上锁，如果上锁则说明前一个处理进程还没有退出
 
 if [[ -e ${lock} ]]
 then
-   echo $filename" is locked"
+   echo $filename is locked
    exit
 fi
 
-echo "unlock continue,adding lock for "$filename
+echo unlock continue,adding lock for $filename
 touch ${lock}
 
 
 if [[  $filename =~ ^([0-9]{18})([0-9]{8})([a-z_]+)\.csv$   ]]
 then
     MD5=`md5sum $filename | awk '{print $1}'`
-    expectMD5=`cat $filename".md5"`
+    expectMD5=`cat $filename.md5`
     echo ${MD5}
     echo ${expectMD5}
     if [[ ${MD5} != ${expectMD5} ]]
     then
-	echo "md5 is error ,exit"
+	echo md5 is error ,exit
     else
-    	echo "continue"
+    	echo continue
     	indate=${BASH_REMATCH[2]}
     	table=${BASH_REMATCH[3]}
-    	echo `date +'%Y-%m-%d %H:%M:%S'`"------load file "${BASH_REMATCH[0]}"\n"  >> `date +%Y%m%d`"log.out"
-    	hivesql="load data inpath '$filename' into table "${table}" PARTTEN BY(indate='${indate}')"
-    	echo ${hivesql}"\n" >> `date +%Y%m%d`"log.out"
-    	hive -e $hivesql
+    	echo `date +%Y-%m-%d %H:%M:%S`------load file ${BASH_REMATCH[0]}  >> `date +%Y%m%d`log.out    	
+    	hivesql=load data inpath '${filename}' into table ${table} PARTTEN BY(indate='${indate}')
+    	echo ${hivesql} >> `date +%Y%m%d`log.out
+    	hive -e "${hivesql}"
     	mv $filename >> /opt/backer/data/
     	###
     fi
 else
-    echo `date +'%Y-%m-%d %H:%M:%S'`"------"$filename" Not Match\n"  >>  `date +%Y%m%d`"error.out"
+    echo `date +%Y-%m-%d %H:%M:%S`------${filename} Not Match\n  >>  `date +%Y%m%d`error.out
 fi
 
 ## 
-echo "end handle!!! deleting lock for "$filename
+echo end handle!!! deleting lock for ${filename}
 rm -rf ${lock}
 
--- shell/mysql/scan_csv.sh
+++ shell/mysql/scan_csv.sh
@@ -5,8 +5,10 @@ datapath=/opt/data1
 for dir in `ls $datapath`
 do
    echo $dir
-   filename=`ls $datapath"/"$dir"/" | grep 'csv$'`
-   sh BASH_REMATCH.sh ${datapath}"/"${dir}"/"${filename} ${filename}
+   for filename in `ls ${datapath}/${dir}/ | grep csv$`
+   do
+   	sh import_csv_to_hive.sh ${datapath}/${dir}/${filename} ${filename}
+   done
 done
 
 
